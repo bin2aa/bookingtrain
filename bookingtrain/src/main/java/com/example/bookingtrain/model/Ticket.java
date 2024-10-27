9e20c352933c0e0c9089bf7498da9fea9c9a935b
ConKienHuy <kiengianghuy1989@gmail.com>
2024-10-27 16:34:38 +0700

    Thêm giao diện admin

--- bookingtrain/src/main/java/com/example/bookingtrain/model/Ticket.java
+++ bookingtrain/src/main/java/com/example/bookingtrain/model/Ticket.java
@@ -2,15 +2,16 @@ package com.example.bookingtrain.model;
 
 import javax.persistence.*;
 import java.util.Date;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
+
+import lombok.*;
 
 @Entity
 @Table(name = "tickets")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,6 +24,12 @@ public class Ticket {
     private Integer legId;
 
     @Column(nullable = false)
+    private Integer stationDepartureId;
+
+    @Column(nullable = false)
+    private Integer stationArrivalId;
+
+    @Column(nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
     private Date timeOfBooking;
 
@@ -33,4 +40,13 @@ public class Ticket {
     @ManyToOne
     @JoinColumn(name = "legId", insertable = false, updatable = false)
     private Leg leg;
+
+    @ManyToOne
+    @JoinColumn(name = "stationDepartureId", referencedColumnName  = "stationId", insertable = false, updatable = false)
+    private Station stationDepartured;
+
+    @ManyToOne
+    @JoinColumn(name = "stationArrivalId", referencedColumnName  = "stationId", insertable = false, updatable = false)
+    private Station stationArrival;
+
 }
--- bookingtrain/src/main/java/com/example/bookingtrain/service/TicketService.java
+++ bookingtrain/src/main/java/com/example/bookingtrain/service/TicketService.java
@@ -1,5 +1,49 @@
 package com.example.bookingtrain.service;
 
+import com.example.bookingtrain.model.Ticket;
+import com.example.bookingtrain.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TicketService {
 
+    private TicketRepository repo;
+//    private Passenger
+
+    @Autowired
+    public TicketService(TicketRepository ticketRepository) {
+        this.repo = ticketRepository;
+    }
+
+    // CRUD
+    public Ticket getByID(int id) {
+        return repo.findById((long) id).orElse(null);
+    }
+
+    public List<Ticket> getAllTicket() {
+        return repo.findAll();
+    }
+
+    public Ticket create(Ticket ticket) {
+        return repo.save(ticket);
+    }
+
+    public Ticket update(int id, Ticket ticket) {
+        Ticket ticketTemp = getByID(id);
+        if (ticketTemp != null) {
+            ticketTemp.setPassenger(ticket.getPassenger());
+            ticketTemp.setLegId(ticket.getLegId());
+            ticketTemp.setStationDepartureId(ticket.getStationDepartureId());
+            ticketTemp.setStationArrivalId(ticket.getStationArrivalId());
+            ticketTemp.setTimeOfBooking(ticket.getTimeOfBooking());
+        }
+        return repo.save(ticket);
+    }
+
+    public void delete(int id) {
+        repo.deleteById((long) id);
+    }
 }
