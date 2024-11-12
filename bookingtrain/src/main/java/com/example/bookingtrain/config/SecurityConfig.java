7adc965511656d3903c98e782552dbcc9269130b
ConKienHuy <kiengianghuy1989@gmail.com>
2024-11-12 17:50:07 +0700

    Huy

--- bookingtrain/src/main/java/com/example/bookingtrain/config/SecurityConfig.java
+++ bookingtrain/src/main/java/com/example/bookingtrain/config/SecurityConfig.java
@@ -1,37 +1,59 @@
-// package com.example.bookingtrain.config;
+package com.example.bookingtrain.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import
-// org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import
-// org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.security.web.SecurityFilterChain;
+import net.bytebuddy.asm.MemberSubstitution;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import
+        org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import
+        org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-// @Configuration
-// @EnableWebSecurity
-// public class SecurityConfig {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
 
-// @Bean
-// public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
-// Exception {
-// http
-// .authorizeRequests(authorizeRequests -> authorizeRequests
-// .antMatchers("/login", "/register", "/css/**", "/js/**").permitAll()
-// .anyRequest().authenticated())
-// .formLogin(formLogin -> formLogin
-// .loginPage("/login")
-// .permitAll())
-// .logout(logout -> logout
-// .permitAll());
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-// return http.build();
-// }
+        http
+                .authorizeRequests(configurer -> configurer
+                        .antMatchers("/css/**", "/js/**", "/images/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests(authorizeRequests -> authorizeRequests
+//                        .antMatchers("/login", "/register", "/").permitAll()
+//                        .anyRequest().authenticated())
+//                .formLogin(formLogin -> formLogin
+//                        .loginPage("/login")
+//                        .permitAll())
+////                .formLogin(wh)
+//                .logout(logout -> logout
+//                        .permitAll());
+//
+//        return http.build();
+//    }
 
 // @Bean
 // public PasswordEncoder passwordEncoder() {
 // return new BCryptPasswordEncoder();
 // }
 // }
+}
