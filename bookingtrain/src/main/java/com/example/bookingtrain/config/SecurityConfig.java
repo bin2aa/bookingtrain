1dfda306012716017969e221a9f1fd493aaf999d
ConKienHuy <kiengianghuy1989@gmail.com>
2024-11-18 13:24:00 +0700

    Okela

--- bookingtrain/src/main/java/com/example/bookingtrain/config/SecurityConfig.java
+++ bookingtrain/src/main/java/com/example/bookingtrain/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package com.example.bookingtrain.config;
 
+import com.example.bookingtrain.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
@@ -20,11 +26,40 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/css/**","/js/**","/img/**").permitAll()
-                .antMatchers("/home/**").permitAll() // Cho phép truy cập tất cả các URL mà không cần đăng nhập
-                .antMatchers("/").hasRole("ADMIN")
-                .formLogin
-                .anyRequest().authenticated()
+                .antMatchers("/home/**").permitAll() // User, Admin truy cập được /home/
+                .antMatchers("/**","/admin/**").hasRole("ADMIN") // Admin truy cập đưọc / , /admin
+                .and() // Neu chua dang nhập mà truy cap trang admin thi show form login
+                .formLogin()
+                    .loginPage("/login")
+                    .usernameParameter("email")
+                    .passwordParameter("password")
+                    .defaultSuccessUrl("/home", true)
+                    .permitAll()
+                .and()
+                .logout()
+                    .permitAll()
+//                .anyRequest().authenticated()
                 .and()
                 .csrf().disable(); // Tắt CSRF để đơn giản hóa cấu hình
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
 }
---
+++ bookingtrain/src/main/java/com/example/bookingtrain/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.bookingtrain.service;
+
+import com.example.bookingtrain.DTO.CustomUserDetails;
+import com.example.bookingtrain.model.User;
+import com.example.bookingtrain.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    // Authicated bằng JPA thi su dung UserDetailServie
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Could not find user");
+        }
+
+        // DTO/CustomUserDetails
+        return new CustomUserDetails(user);
+    }
+
+}
