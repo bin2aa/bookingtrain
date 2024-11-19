d70a5db5181c7a146822de677345e4799ace7480
ConKienHuy <kiengianghuy1989@gmail.com>
2024-11-19 23:16:49 +0700

    Lẩu thập cẩm

--- bookingtrain/src/main/java/com/example/bookingtrain/controller/AccountInfoController.java
+++ bookingtrain/src/main/java/com/example/bookingtrain/controller/AccountInfoController.java
@@ -14,7 +14,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDate;
 import java.util.List;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 @Controller
 @RequestMapping("/account")
@@ -35,9 +38,14 @@ public class AccountInfoController {
     @GetMapping("")
     public String accountInfo(Model model, HttpSession session) {
         User user = (User)session.getAttribute("user");
-        Integer userId = user.getUserId();
-        Employee employee = new Employee();
-        if(userId != null) employee = employeeService.findByUserId(userId);
+        Employee employee = employeeService.findByUserId(user.getUserId());
+        if(employee == null) {
+            // Chú bé mới đăng nhập thì bắt nhập đu thông tin
+            // tại AccountInfo.html
+            // nhập thếu nhốt ko cho ra
+            employee = new Employee();
+        }
+
         List<Role> roleList = roleService.getAllRoles();
         model.addAttribute("user", user);
         model.addAttribute("employee", employee);
@@ -46,16 +54,27 @@ public class AccountInfoController {
     }
 
     @PreAuthorize("isAuthenticated()")
-    @PostMapping("/updateAccount")
+    @PostMapping("/updateAccount/{requestFrom}")
     public String accountInfo(@ModelAttribute("user") User user,
                               @ModelAttribute("employee") Employee employee,
+                              @PathVariable("requestFrom") String requestFrom,
                               RedirectAttributes redirectAttributes, Model model) {
-//        User existingUser = userService.getUserById(user.getUserId());
-//        existingUser.setEmail(user.getEmail());
-//        existingUser.setPassword(user.getPassword());
-//        existingUser.setUsername(user.getUsername());
 
         Employee existingEmployee = employeeService.findByUserId(user.getUserId());
+        if(existingEmployee == null){
+            // Nếu chưa có dữ liệu trên database thì gán tạo mới employee
+            // và gán userId
+            existingEmployee = new Employee();
+            existingEmployee.setUserId(user.getUserId());
+        }
+        boolean isComeFromFuture = DAYS.between(employee.getDateOfBirth(), LocalDate.now()) <= 0 ? true : false;
+        if(isComeFromFuture ){
+            // Thanh nin đến từ tương lai
+            redirectAttributes.addFlashAttribute("message", "Vui lòng cung cấp ngày sinh phù họp.");
+            model.addAttribute("user", user);
+            model.addAttribute("employee", employee);
+            return "redirect:/account/updateAccount";
+        }
         existingEmployee.setEmployeeName(employee.getEmployeeName());
         existingEmployee.setAddress(employee.getAddress());
         existingEmployee.setPhone(employee.getPhone());
@@ -67,13 +86,34 @@ public class AccountInfoController {
         model.addAttribute("user", updatedUser);
         model.addAttribute("employee", updatedEmployee);
         redirectAttributes.addFlashAttribute("message","Information updated successfully");
-        return "redirect:/account";
+        return requestFrom.equals("1") ? "redirect:/account/admin" : "redirect:/account";
     }
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/deleteAccount/{id}")
-    public String deleteAccount(@PathVariable Integer id) {
+    public String deleteAccount(@PathVariable Integer id, @PathVariable String requestFrom) {
+        // admin tu xoa thi sao nhi?
         userService.deleteUser(id);
         return "redirect:/home";
     }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/admin")
+    public String showAdminAccount(Model model, HttpSession session) {
+        User user = (User)session.getAttribute("user");
+        Employee employee = employeeService.findByUserId(user.getUserId());
+        if(employee == null) {
+            // Chú bé mới đăng nhập thì bắt nhập đu thông tin
+            // tại AccountInfo.html
+            // nhập thếu nhốt ko cho ra
+            employee = new Employee();
+        }
+
+        List<Role> roleList = roleService.getAllRoles();
+        model.addAttribute("user", user);
+        model.addAttribute("employee", employee);
+        model.addAttribute("roleList", roleList);
+        return "/admin/AccountInfoAdmin";
+    }
+
 }
