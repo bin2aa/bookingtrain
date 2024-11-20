093690b38be24e1a2b4cdd918de52c84a06a6fd1
tuansgu <nguyentuan01082003t@gmail.com>
2024-11-20 12:31:18 +0700

    upadte schedule and feature searchticket

--- bookingtrain/src/main/java/com/example/bookingtrain/repository/InfoReservationRepository.java
+++ bookingtrain/src/main/java/com/example/bookingtrain/repository/InfoReservationRepository.java
@@ -26,18 +26,24 @@ public class InfoReservationRepository {
 
         String sql = "SELECT " +
                 "tr.trainId AS trainId, " +
+                "sc.scheduleId AS scheduleId, " +
                 "sd.stationName AS Departure, " +
                 "sa.stationName AS Arrival, " +
                 "tr.trainName AS trainCode, " +
                 "DATE(sc.startDeparture) AS DepartureDate, " +
                 "TIME(sc.startDeparture) AS DepartureTime, " +
                 "DATE(sc.endDeparture) AS ArrivalDate, " +
-                "TIME(sc.endDeparture) AS ArrivalTime " +
+                "TIME(sc.endDeparture) AS ArrivalTime, " +
+                "TIMEDIFF(sc.endDeparture, sc.startDeparture) AS totalTime, " +
+                "sdc.stationName AS ScheduleDeparture, " +
+                "sac.stationName AS ScheduleArrival " +
                 "FROM schedules sc " +
                 "JOIN trains tr ON sc.trainId = tr.trainId " +
                 "JOIN routes ro ON ro.trainId = tr.trainId " +
                 "JOIN stations sd ON sd.stationId = ro.stationDepartureId " +
                 "JOIN stations sa ON sa.stationId = ro.stationArrivalId " +
+                "JOIN stations sdc ON sdc.stationId = sc.stationDepartureId " +
+                "JOIN stations sac ON sac.stationId = sc.stationArrivalId " +
                 "WHERE ro.stationDepartureId = ? " +
                 "AND ro.stationArrivalId = ? " +
                 "AND DATE(sc.startDeparture) = ? " +
@@ -57,6 +63,7 @@ public class InfoReservationRepository {
         List<InforReservationDTO> tickets = jdbcTemplate.query(sql, (rs, rowNum) -> {
             InforReservationDTO ticketInfo = new InforReservationDTO();
             ticketInfo.setTrainId(rs.getInt("trainId"));
+            ticketInfo.setScheduleId(rs.getInt("scheduleId"));
             ticketInfo.setStationDeparture(rs.getString("Departure"));
             ticketInfo.setStationArrival(rs.getString("Arrival"));
             ticketInfo.setTrainCode(rs.getString("trainCode"));
@@ -64,6 +71,9 @@ public class InfoReservationRepository {
             ticketInfo.setTimeDepartureFormatted(rs.getString("DepartureTime"));
             ticketInfo.setDateArrivalFormatted(rs.getString("ArrivalDate"));
             ticketInfo.setTimeArrivalFormatted(rs.getString("ArrivalTime"));
+            ticketInfo.setScheduleDeparture(rs.getString("ScheduleDeparture"));
+            ticketInfo.setScheduleArrival(rs.getString("ScheduleArrival"));
+            ticketInfo.setTotalTime(rs.getString("TotalTime"));
             return ticketInfo;
         }, stationDepartureId, stationArrivalId, dateStart,
                 pageable.getPageSize(), pageable.getOffset());
